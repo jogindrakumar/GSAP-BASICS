36404c4f9bf4e9129935f02c12e6ff0fdcea4abf
jogindrakumar <joginder9868@outlook.com>
2024-01-18 20:40:03 +0530

    basics

---
+++ script.js
@@ -0,0 +1,26 @@
+var tl = gsap.timeline()
+tl.to("#box1",{
+    x:1000,
+    duration:2,
+    rotate:360,
+   borderRadius:"50%",
+  
+    delay:1
+})
+tl.to("#box2",{
+    x:1000,
+    duration:1,
+    rotate:360,
+   borderRadius:"50%",
+   
+   
+
+})
+tl.to("#box3",{
+    x:1000,
+    duration:1,
+    rotate:360,
+   borderRadius:"50%",
+    
+
+})
